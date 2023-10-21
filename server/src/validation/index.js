50667f5ec2505cfe38cf80c2e8bedbd62fa5d5aa
S520H0531 <520H0531@student.tdtu.edu.vn>
2023-10-21 14:14:58 +0700

    fix conflict validation

--- server/src/validation/index.js
+++ server/src/validation/index.js
@@ -1,14 +1,11 @@
 const Joi = require('joi')
 
 module.exports = {
-<<<<<<< HEAD
-=======
 	registerSchema: Joi.object({
 		email: Joi.string().email().required(),
 		role: Joi.string().valid('seller', 'admin', 'management').required(),
 	}),
 
->>>>>>> develop
 	loginSchema: Joi.object({
 		username: Joi.string().min(3).max(20).required(),
 		password: Joi.string().min(3).max(20).required(),
