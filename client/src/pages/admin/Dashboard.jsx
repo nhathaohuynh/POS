fed813f0d77a5f1a2e9dc6934f12882fa9670506
S520H0531 <520H0531@student.tdtu.edu.vn>
2023-10-21 14:19:22 +0700

    fix conlict dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 const Dashboard = () => {
-<<<<<<< HEAD
-=======
 	const { user, isLogin } = useSelector((state) => state.authReducer)
 	useEffect(() => {
 		if (!isLogin && user?.role !== 'admin') {
 			logout()
 		}
 	}, [user, isLogin])
->>>>>>> develop
 	return <div className='w-full h-screen flex text-white'></div>
 }
 
